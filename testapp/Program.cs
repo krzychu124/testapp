e9a8174cfab8418e6f50f8842a9c4b2877d0dfb0
krzychu124 <krzychu124@gmail.com>
2019-03-10 00:08:26 +0100

    Added StyleCop analyzer

--- testapp/Program.cs
+++ testapp/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Testapp {
-    class Program {
-        static void Main(string[] args) {
+    internal class Program {
+        private static void Main(string[] args) {
             Console.WriteLine("Console writeLine test");
             Console.WriteLine("Moar lines");
             Console.ReadKey();
