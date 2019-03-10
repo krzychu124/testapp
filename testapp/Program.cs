f160cc380f65004a2eedee6e71143867df351baf
krzychu124 <krzychu124@gmail.com>
2019-03-10 03:17:13 +0100

    Fixed warnings, added lib test

--- testapp/Program.cs
+++ testapp/Program.cs
@@ -7,6 +7,11 @@ namespace Testapp {
             Console.WriteLine("Moar lines");
             Console.ReadKey();
             var u = "test";
+            UnityEngine.AnisotropicFiltering anisoTest = UnityEngine.AnisotropicFiltering.ForceEnable;
+            if (anisoTest.Equals(UnityEngine.AnisotropicFiltering.ForceEnable)) {
+                Console.WriteLine("Aniso enabled");
+                Console.WriteLine(u);
+            }
         }
     }
 }
